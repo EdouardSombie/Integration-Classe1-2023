c03a9211472099a9c22f75085e9821104b87e245
Edouard <edouard@voltajazz.com>
2024-01-25 11:12:47 +0100

    Fin séance vite (navbar terminée)

--- Framework - vite/IW1/vite.config.js
+++ Framework - vite/IW1/vite.config.js
@@ -6,7 +6,25 @@ export default defineConfig({
 		lib: {
 			entry: path.resolve(__dirname, "./src/js/main.js"),
 			name: "ESGI_framework",
-			//formats: ["es"],
+			formats: ["es"],
+		},
+		rollupOptions: {
+			output: {
+				entryFileNames: "js/[name].js",
+				assetFileNames: ({ name }) => {
+					// Ne sera pas utilisé dans le cadre d'une génération de library...
+					if (/\.(gif|jpe?g|png|svg)$/.test(name ?? "")) {
+						return "images/[name][extname]";
+					}
+					if (/\.css$/.test(name ?? "")) {
+						// return "assets/css/[name]-[hash][extname]";
+						return "css/[name][extname]";
+					}
+					// default value
+					// ref: https://rollupjs.org/guide/en/#outputassetfilenames
+					return "assets/[name]-[hash][extname]";
+				},
+			},
 		},
 	}, // ...
 });
